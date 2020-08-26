14608c9aebb9408b9c4fdbb7e1593c9cc75091c1
mitsushi.kohara <mitsushi.kohara@gr.tsubakimoto.co.jp>
2020-08-26 10:44:06 +0900

    コメント除去
    コメント除去

--- src/main/java/com/tsubaki/dm/controller/ImageController.java
+++ src/main/java/com/tsubaki/dm/controller/ImageController.java
@@ -7,23 +7,18 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.tsubaki.dm.util.DownloadImage;
 
 @Secured("IS_AUTHENTICATED_FULLY")
-//@RestController
 @Controller
 public class ImageController {
 	
 	@Autowired
 	DownloadImage downloadImage;
-//	@Autowired
-//	PrintView printView;
 
     @ResponseBody
 	@RequestMapping(value = "/getImage")
